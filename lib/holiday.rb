d6d4b3f7258ef5a8a8d964f226e346a1b3c5f4b6
David Hutchison <dhutchis68516@gmail.com>
2020-05-08 19:52:18 +0000

    Automatically backed up by Learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -20,9 +20,10 @@ def second_supply_for_fourth_of_july(holiday_hash)
   # return the second element in the 4th of July array
   holiday_hash.each do |season, holiday|
     if season == :summer
+      binding.pry
       holiday.each do |fourth, stuff|
         stuff[1]
-        binding.pry
+
       end
     end
   end
